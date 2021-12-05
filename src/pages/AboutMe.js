4daea8c21bbeb9b0c0cdc9d4984221d365a0a0ba
sws2apps <sws2apps@gmail.com>
2021-12-05 03:10:20 +0300

    Clean-up package json

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -5,11 +5,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
 import Typography from "@mui/material/Typography";
-import { appVersionState, settingsState } from "../appStates/appSettings";
+import { settingsState } from "../appStates/appSettings";
 
 const AboutMe = () => {
     const appSettings = useRecoilValue(settingsState);
-    const appVersion = useRecoilValue(appVersionState);
 
     const congID = appSettings.cong_id;
     const congPIN = appSettings.cong_PIN;
