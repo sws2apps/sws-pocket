95d2b2dd78743dce0ded213e4534fb60dc6f205f
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-08-13 12:40:48 +0300

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 	define: {
 		'process.env': process.env,
 	},
-	plugins: [react(), eslint({ exclude: 'node_modules'}), loadVersion()],
+	plugins: [react(), eslint({ exclude: '/node_modules/'}), loadVersion()],
 	build: {
 		chunkSizeWarningLimit: 1500,
 	},
