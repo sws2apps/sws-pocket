dc42313e18c8e8c337762a720ff5d2d961c11d11
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-11-25 19:43:33 +0300

    feat(schedules): fetch on startup automatically

--- src/components/root/ScheduleUpdater.jsx
+++ src/components/root/ScheduleUpdater.jsx
@@ -1,128 +1,106 @@
-import { useCallback, useEffect, useRef } from 'react';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useEffect, useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useTranslation } from 'react-i18next';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import IconButton from '@mui/material/IconButton';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import Typography from '@mui/material/Typography';
-import {
-	apiHostState,
-	isOnlineState,
-	isRefreshScheduleOpenState,
-	visitorIDState,
-} from '../../states/app';
-import {
-	appMessageState,
-	appSeverityState,
-	appSnackOpenState,
-} from '../../states/notification';
+import { apiHostState, isOnlineState, visitorIDState } from '../../states/app';
+import { appMessageState, appSeverityState, appSnackOpenState } from '../../states/notification';
 import { dbUpdateSchedule } from '../../indexedDb/appSchedule';
 
 const ScheduleUpdater = () => {
-	const { t } = useTranslation();
-	const abortCont = useRef();
+  const { t } = useTranslation();
 
-	const [open, setOpen] = useRecoilState(isRefreshScheduleOpenState);
+  const [open, setOpen] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-	const setAppSnackOpen = useSetRecoilState(appSnackOpenState);
-	const setAppSeverity = useSetRecoilState(appSeverityState);
-	const setAppMessage = useSetRecoilState(appMessageState);
+  const setAppSnackOpen = useSetRecoilState(appSnackOpenState);
+  const setAppSeverity = useSetRecoilState(appSeverityState);
+  const setAppMessage = useSetRecoilState(appMessageState);
 
-	const isOnline = useRecoilValue(isOnlineState);
-	const apiHost = useRecoilValue(apiHostState);
-	const visitorID = useRecoilValue(visitorIDState);
+  const isOnline = useRecoilValue(isOnlineState);
+  const apiHost = useRecoilValue(apiHostState);
+  const visitorID = useRecoilValue(visitorIDState);
 
-	const refreshSchedule = useCallback(async () => {
-		let abortPossible = false;
-		try {
-			abortCont.current = new AbortController();
-			if (open && isOnline && apiHost !== '') {
-				setOpen(true);
+  const handleManualRefresh = () => {
+    setOpen((prev) => {
+      return !prev;
+    });
+  };
 
-				const res = await fetch(`${apiHost}api/sws-pocket/schedule`, {
-					signal: abortCont.current.signal,
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-						visitorid: visitorID,
-					},
-				});
+  useEffect(() => {
+    const abortCont = new AbortController();
 
-				abortPossible = true;
+    const refreshSchedule = async () => {
+      try {
+        if (isOnline && apiHost !== '') {
+          setIsLoading(true);
 
-				const data = await res.json();
+          const res = await fetch(`${apiHost}api/sws-pocket/schedule`, {
+            signal: abortCont.signal,
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              visitorid: visitorID,
+            },
+          });
 
-				if (res.status === 200) {
-					await dbUpdateSchedule(data);
-					setOpen(false);
-					return;
-				}
+          const data = await res.json();
 
-				setOpen(false);
-				setAppMessage(data.message);
-				setAppSeverity('warning');
-				setAppSnackOpen(true);
-			}
-		} catch (err) {
-			if (abortPossible && !abortCont.current.signal.aborted) {
-				setOpen(false);
-				setAppMessage(err.message);
-				setAppSeverity('error');
-				setAppSnackOpen(true);
-			}
-		}
-	}, [
-		abortCont,
-		apiHost,
-		isOnline,
-		open,
-		setAppMessage,
-		setAppSeverity,
-		setAppSnackOpen,
-		setOpen,
-		visitorID,
-	]);
+          if (res.status === 200) {
+            await dbUpdateSchedule(data);
+            setIsLoading(false);
+            return;
+          }
 
-	useEffect(() => {
-		refreshSchedule();
-	}, [refreshSchedule]);
+          setAppMessage(data.message);
+          setAppSeverity('warning');
+          setAppSnackOpen(true);
+          setIsLoading(false);
+        }
+      } catch (err) {
+        if (!abortCont.signal.aborted) {
+          setAppMessage(err.message);
+          setAppSeverity('error');
+          setAppSnackOpen(true);
+          setIsLoading(false);
+        }
+      }
+    };
 
-	useEffect(() => {
-		return () => {
-			if (abortCont.current) {
-				setOpen(false);
-				abortCont.current.abort();
-			}
-		};
-	}, [abortCont, setOpen]);
+    refreshSchedule();
 
-	return (
-		<Box
-			sx={{
-				display: 'flex',
-				justifyContent: 'flex-end',
-				boxSizing: 'border-box',
-				alignItems: 'center',
-				marginRight: '10px',
-				height: '40px',
-			}}
-		>
-			{open && (
-				<>
-					<Typography sx={{ marginRight: '10px', fontSize: '12px' }}>
-						{t('scheduleRefreshProgress')}
-					</Typography>
-					<CircularProgress disableShrink color='secondary' size={'20px'} />
-				</>
-			)}
-			{!open && (
-				<IconButton onClick={() => setOpen(true)}>
-					<RefreshIcon />
-				</IconButton>
-			)}
-		</Box>
-	);
+    return () => {
+      abortCont.abort();
+    };
+  }, [apiHost, isOnline, open, setAppMessage, setAppSeverity, setAppSnackOpen, visitorID]);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        boxSizing: 'border-box',
+        alignItems: 'center',
+        marginRight: '10px',
+        height: '40px',
+      }}
+    >
+      {isLoading && (
+        <>
+          <Typography sx={{ marginRight: '10px', fontSize: '12px' }}>{t('scheduleRefreshProgress')}</Typography>
+          <CircularProgress disableShrink color='secondary' size={'20px'} />
+        </>
+      )}
+      {!isLoading && (
+        <IconButton onClick={handleManualRefresh}>
+          <RefreshIcon />
+        </IconButton>
+      )}
+    </Box>
+  );
 };
 
 export default ScheduleUpdater;
