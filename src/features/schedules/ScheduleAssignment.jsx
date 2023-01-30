c5445d31c664296b58df01a63e77bd21d8b49796
rhahao <26148770+rhahao@users.noreply.github.com>
2023-01-30 20:20:13 +0300

    single override bug

--- src/features/schedules/ScheduleAssignment.jsx
+++ src/features/schedules/ScheduleAssignment.jsx
@@ -134,22 +134,27 @@ const ScheduleAssignment = ({ edit }) => {
       setAss4A(schedule.ass4_ass_A_dispName);
       setStu4B(schedule.ass4_stu_B_dispName);
       setAss4B(schedule.ass4_ass_B_dispName);
-      if (schedule.lcCount_override) {
-        setLcCount(schedule.lcCount_override);
+      if (schedule.lcCount_override) setLcCount(schedule.lcCount_override);
+      if (!schedule.lcCount_override) setLcCount(schedule.lcCount);
+      if (schedule.lcPart1_time_override) {
         setLcPart1Time(schedule.lcPart1_time_override);
-        setLcPart1Src(schedule.lcPart1_src_override ? schedule.lcPart1_src_override[lang] : '');
-        setLcPart1Content(schedule.lcPart1_content_override ? schedule.lcPart1_content_override[lang] : '');
-        setLcPart2Time(schedule.lcPart2_time_override);
-        setLcPart2Src(schedule.lcPart2_src_override ? schedule.lcPart2_src_override[lang] : '');
-        setLcPart2Content(schedule.lcPart2_content_override ? schedule.lcPart2_content_override[lang] : '');
-      } else {
-        setLcCount(schedule.lcCount);
+        setLcPart1Src(schedule.lcPart1_src_override);
+        setLcPart1Content(schedule.lcPart1_content_override);
+      }
+      if (!schedule.lcPart1_time_override) {
         setLcPart1Time(schedule.lcPart1_time);
-        setLcPart1Src(schedule.lcPart1_src ? schedule.lcPart1_src[lang] : '');
-        setLcPart1Content(schedule.lcPart1_content ? schedule.lcPart1_content[lang] : '');
+        setLcPart1Src(schedule.lcPart1_src);
+        setLcPart1Content(schedule.lcPart1_content);
+      }
+      if (schedule.lcPart2_time_override) {
+        setLcPart2Time(schedule.lcPart2_time_override);
+        setLcPart2Src(schedule.lcPart2_src_override);
+        setLcPart2Content(schedule.lcPart2_content_override);
+      }
+      if (!schedule.lcPart2_time_override) {
         setLcPart2Time(schedule.lcPart2_time);
-        setLcPart2Src(schedule.lcPart2_src ? schedule.lcPart2_src[lang] : '');
-        setLcPart2Content(schedule.lcPart2_content ? schedule.lcPart2_content[lang] : '');
+        setLcPart2Src(schedule.lcPart2_src);
+        setLcPart2Content(schedule.lcPart2_content);
       }
       setLcPart1(schedule.lc_part1_dispName);
       setLcPart2(schedule.lc_part2_dispName);
